3a4189d032aa5f1af05fba09cdd9195b6fd5bf12
joshua stein <jcs@jcs.org>
2011-09-19 22:31:11 -0500

    initial version of my font and an xterm helper script

---
+++ ansiterm.sh
@@ -0,0 +1,25 @@
+# colors 0-7 are black, red, green, yellow, blue, magenta, cyan, gray
+# colors 8-15 are bold versions
+
+xterm \
+	-g 80x24 \
+	-fn dos437 \
+	-bg black \
+	-fg "#aaaaaa" \
+	-xrm "*boldMode: false" \
+	-xrm "*color0: #000000" \
+	-xrm "*color1: #aa0000" \
+	-xrm "*color2: #00aa00" \
+	-xrm "*color3: #aa5600" \
+	-xrm "*color4: #0000aa" \
+	-xrm "*color5: #aa00aa" \
+	-xrm "*color6: #00aaaa" \
+	-xrm "*color7: #aaaaaa" \
+	-xrm "*color8: #555555" \
+	-xrm "*color9: #fe5656" \
+	-xrm "*color10: lime" \
+	-xrm "*color11: #fefe56" \
+	-xrm "*color12: #5656fe" \
+	-xrm "*color13: magenta" \
+	-xrm "*color14: #56fefe" \
+	-xrm "*color15: white"
